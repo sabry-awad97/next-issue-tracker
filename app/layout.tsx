22d1505ed58770e88f5183bf1f3140a2864ca91e
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-11 20:01:50 +0200

    🎨 style(layout.tsx): Update Theme component invocation with appearance and accentColor props

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Theme>
+        <Theme appearance="light" accentColor="violet">
           <Navbar />
           <main className="p-5">{children}</main>
         </Theme>
