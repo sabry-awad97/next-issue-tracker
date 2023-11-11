0eddda9a6f7c79fcabff375af0d42634fe8e19be
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-11 19:35:10 +0200

    🎨 style(app): Enhance UI with Radix UI button in IssuesPage 🚀 chore(app): Add Radix UI Themes dependency in package.json 🔧 chore(app): Integrate Radix UI Theme in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import { Theme } from '@radix-ui/themes';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from './components/unique/Navbar';
+
+import '@radix-ui/themes/styles.css';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,8 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
+        <Theme>
+          <Navbar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
