8204cc7b2b20d5fb1455587cca1132775025f71b
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-12 11:37:23 +0200

    🎨 style(page.tsx): Improve layout and add 'Edit Issue' button 📦 chore(package.json): Update @radix-ui/react-icons to version 1.3.0

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import IssueStatusBadge from '@/app/components/shared/IssueStatusBadge';
 import prisma from '@/prisma/client';
-import { Box, Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import Markdown from 'react-markdown';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 interface Props {
   params: {
@@ -22,16 +24,24 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
   }
 
   return (
-    <Box>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="5" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        <Markdown>{issue.description}</Markdown>
-      </Card>
-    </Box>
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="5" my="2">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <Markdown>{issue.description}</Markdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
