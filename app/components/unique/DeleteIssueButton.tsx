1efb4970bb54c0c0584af02884cbe803410bd337
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-12 16:25:20 +0200

    🔄 refactor(DeleteIssueButton.tsx): Implement confirmation dialog for issue deletion

--- app/components/unique/DeleteIssueButton.tsx
+++ app/components/unique/DeleteIssueButton.tsx
@@ -1,13 +1,36 @@
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
-import Link from 'next/link';
+'use client';
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import React from 'react';
 
 const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
   return (
-    <Button color="red">
-      <Link href={`/issues/${issueId}/delete`}>Delete Issue</Link>
-    </Button>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content style={{ maxWidth: 450 }}>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure you want to delete this issue? This action cannot be
+            undone.
+          </AlertDialog.Description>
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red">
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
