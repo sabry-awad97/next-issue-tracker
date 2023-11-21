e3a65ae620665e8762accdd5cba29e1d36882cf0
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-21 12:50:47 +0200

    ✨ feat(auth): Add authentication provider component 🔄 refactor(components): Update Navbar to include authentication links 🔄 refactor(layout): Wrap layout with AuthProvider for session management

--- app/components/unique/Navbar.tsx
+++ app/components/unique/Navbar.tsx
@@ -4,9 +4,12 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
 import NavbarLink from './NavbarLink';
+import { useSession } from 'next-auth/react';
+import { Box } from '@radix-ui/themes';
 
 const Navbar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   const navLinks = [
     { href: '/', text: 'Dashboard' },
@@ -28,6 +31,14 @@ const Navbar = () => {
           />
         ))}
       </ul>
+      <Box>
+        {status === 'authenticated' && (
+          <Link href="/api/auth/signout">Logout</Link>
+        )}
+        {status === 'unauthenticated' && (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
+      </Box>
     </nav>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from './components/unique/Navbar';
 import '@radix-ui/themes/styles.css';
 import './theme-config.css';
 import './globals.css';
+import AuthProvider from './auth/Provider';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -25,12 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.variable}>
-        <Theme appearance="light" accentColor="violet">
-          <Navbar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme appearance="light" accentColor="violet">
+            <Navbar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
