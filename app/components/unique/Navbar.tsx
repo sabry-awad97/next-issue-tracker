410b5e506b1f7a043208adeba02ff41552019aca
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-21 13:23:27 +0200

    🔄 refactor(Navbar.tsx): Improve Navbar layout with Radix UI components 🚀

--- app/components/unique/Navbar.tsx
+++ app/components/unique/Navbar.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
 import NavbarLink from './NavbarLink';
 import { useSession } from 'next-auth/react';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 
 const Navbar = () => {
   const currentPath = usePathname();
@@ -17,28 +17,34 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {navLinks.map(link => (
-          <NavbarLink
-            key={link.href}
-            href={link.href}
-            text={link.text}
-            currentPath={currentPath}
-          />
-        ))}
-      </ul>
-      <Box>
-        {status === 'authenticated' && (
-          <Link href="/api/auth/signout">Logout</Link>
-        )}
-        {status === 'unauthenticated' && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-      </Box>
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug />
+            </Link>
+            <ul className="flex space-x-6">
+              {navLinks.map(link => (
+                <NavbarLink
+                  key={link.href}
+                  href={link.href}
+                  text={link.text}
+                  currentPath={currentPath}
+                />
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === 'authenticated' && (
+              <Link href="/api/auth/signout">Logout</Link>
+            )}
+            {status === 'unauthenticated' && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
