46592fb039ca5ad51dec72be256ca6e8e196a363
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-22 15:40:40 +0200

    🎨 style(app/page.tsx): Improve layout using Radix UI Flex and Grid components

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import IssuesSummary from './components/unique/IssuesSummary';
 import LatestIssues from './components/unique/LatestIssues';
 import { Status } from '@prisma/client';
 import IssuesChart from './components/unique/IssuesChart';
+import { Flex, Grid } from '@radix-ui/themes';
 
 const Home = async () => {
   const service = IssueService.getInstance();
@@ -11,11 +12,13 @@ const Home = async () => {
   const closed = await service.getIssuesCount(Status.CLOSED);
 
   return (
-    <>
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssuesSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssuesChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
       <LatestIssues />
-      <IssuesSummary open={open} inProgress={inProgress} closed={closed} />
-      <IssuesChart open={open} inProgress={inProgress} closed={closed} />
-    </>
+    </Grid>
   );
 };
 
